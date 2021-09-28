729546059ffd9b222889618414b4f83f2a7175a1
Kyyx <kmariemagdelaine@myges.fr>
2021-09-28 14:42:32 +0200

    2 joueurs peuvent tirer

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -10,12 +10,13 @@ public class PlayerShoot : MonoBehaviour
 
     public Vector3 shootDirection = new Vector3(1,0,0);
 
+    public int playerNumber;
+
     private float fireRate = 0.6f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     public void ChangeDirection(Vector3 direction)
@@ -26,10 +27,13 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        fireRate -= Time.deltaTime;
+        Debug.Log(fireRate);
+        if (Input.GetAxis("P" + playerNumber + "_Shoot") > 0 && fireRate < 0)
         {
             var newBullet = Instantiate(bullet, new Vector3(shooterPosition.position.x, shooterPosition.position.y, shooterPosition.position.z), Quaternion.Euler(0,0,0));
             BulletManager.Instance().AddBullet(newBullet.transform, shootDirection);
+            fireRate = 0.6f;
         }
         
     }
