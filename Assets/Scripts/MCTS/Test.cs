584c8c31919644b3a26c0b27fe25f5f10799bb35
Kyyx <kmariemagdelaine@myges.fr>
2021-09-28 14:07:32 +0200

    Test MCTS + Menu

--- Assets/Scripts/MCTS/Test.cs
+++ Assets/Scripts/MCTS/Test.cs
@@ -6,13 +6,15 @@ using Random = UnityEngine.Random;
 
 public enum Action
 {
-   Left, Right, Up, Down
+   Left, Right, Up, Down, Shoot
 }
 
 public class Test : MonoBehaviour
 {
     public Transform transformTest;
+    public Transform target;
     public Vector3 destination;
+    public Vector3 targetLastPosition;
     
     public Dictionary<Action, int> allPossibleActionsResult;
     public Dictionary<Action, (int, int)> allPossibleActionsMove;
@@ -20,26 +22,31 @@ public class Test : MonoBehaviour
     void Start()
     {
         allPossibleActionsResult = new Dictionary<Action, int>();
-        allPossibleActionsResult.Add(Action.Left, 0);
-        allPossibleActionsResult.Add(Action.Right, 0);
-        allPossibleActionsResult.Add(Action.Up, 0);
-        allPossibleActionsResult.Add(Action.Down, 0);
+        //allPossibleActionsResult.Add(Action.Left, 0);
+        //allPossibleActionsResult.Add(Action.Right, 0);
+        //allPossibleActionsResult.Add(Action.Up, 0);
+       // allPossibleActionsResult.Add(Action.Down, 0);
+        allPossibleActionsResult.Add(Action.Shoot, 0);
 
         allPossibleActionsMove = new Dictionary<Action, (int, int)>();
-        allPossibleActionsMove.Add(Action.Left, (-1, 0));
-        allPossibleActionsMove.Add(Action.Right, (1, 0));
-        allPossibleActionsMove.Add(Action.Up, (0, 1));
-        allPossibleActionsMove.Add(Action.Down, (0, -1));
+        //allPossibleActionsMove.Add(Action.Left, (-1, 0));
+        //allPossibleActionsMove.Add(Action.Right, (1, 0));
+        //allPossibleActionsMove.Add(Action.Up, (0, 1));
+        //allPossibleActionsMove.Add(Action.Down, (0, -1));
+        allPossibleActionsMove.Add(Action.Shoot, (0, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (transformTest.position != destination)
-        {
-            ComputeMCTS();
-        }
+        
+        Debug.Log("Next Step");
+        ComputeMCTS();
+
+        targetLastPosition = target.position;
+
+
     }
     
     void ComputeMCTS()
@@ -49,9 +56,9 @@ public class Test : MonoBehaviour
         foreach (var possibleAction in allPossibleActionsMove) //Expansion
         {
             int numberVictory = 0;
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 800; i++)
             {
-                numberVictory += SimulateResult(allPossibleActionsMove[possibleAction.Key]); //Simulation (a faire plusieurs fois !)
+                numberVictory += SimulateResult(possibleAction.Key); //Simulation (a faire plusieurs fois !)
             }
             allPossibleActionsResult[possibleAction.Key] = numberVictory; //Retropropagation
             Debug.Log($"{possibleAction.Key} : {numberVictory}");
@@ -61,15 +68,44 @@ public class Test : MonoBehaviour
                 bestAction = possibleAction.Key;
             }
         }
-        transformTest.position += new Vector3(allPossibleActionsMove[bestAction].Item1, allPossibleActionsMove[bestAction].Item2, 0);
+        //transformTest.position += new Vector3(allPossibleActionsMove[bestAction].Item1, allPossibleActionsMove[bestAction].Item2, 0);
     }
     
-    int SimulateResult((int, int) possibleAction)
+    int SimulateResult(Action possibleAction)
     {
-        List<Action> actions = new List<Action>(allPossibleActionsMove.Keys);
+        List<Action> actions = new List<Action>(allPossibleActionsResult.Keys);
         var positionTemp = transformTest.localPosition;
+
+        if (possibleAction == Action.Shoot)
+        {
+            var tH = Math.Abs(positionTemp.x - target.position.x);
+            var tPH = Math.Abs(positionTemp.x - target.position.x) / 5;
+            
+            var xH = 1 * Math.Cos(0) * tH + positionTemp.x;
+            var yH = 1 * Math.Sin(0) * tH + positionTemp.y;
+            
+            var xTH = 5 * Math.Cos(0) * tPH;
+            var yTH = 5 * Math.Sin(0) * tPH;
+            
+            var tV = Math.Abs(positionTemp.y - target.position.y);
+            var tPV = Math.Abs(positionTemp.y - target.position.y) / 5;
+
+            var xTV = 5 * 0 * tPV + target.position.x;
+            var yTV = 5 * 1 * tPV + target.position.y;
+            
+            var xV = 1 * 0 * tV;
+            var yV = 1 * 1 * tV;
+            Debug.Log($"({xH}, {yH}) - ({xTV}, {yTV})");
+            
+            Debug.Log(xH == xTV && yH == yTV);
+        }
+        else
+        {
+            
+        }
         
-        positionTemp += new Vector3(possibleAction.Item1, possibleAction.Item2, 0);
+        
+        /*positionTemp += new Vector3(possibleAction.Item1, possibleAction.Item2, 0);
         
         var result = 1;
         while (positionTemp != destination) //Attention votre jeu doit être fini !
@@ -79,13 +115,20 @@ public class Test : MonoBehaviour
             
             positionTemp += new Vector3(allPossibleActionsMove[actions[selectedAction]].Item1, allPossibleActionsMove[actions[selectedAction]].Item2, 0);
 
-            /*if ()
+            if (-40 > positionTemp.x || positionTemp.x > 40)
             {
-                result++;
+                result = 0;
                 return result;
-            }*/
+            }
+            
+            if (-40 > positionTemp.y || positionTemp.y > 40)
+            {
+                result = 0;
+                return result;
+            }
         }
-        return result; //0 si perdu 1 si win
+        return result; //0 si perdu 1 si win*/
+        return 1;
     }
 
 }
